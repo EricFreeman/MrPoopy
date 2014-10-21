2a7969cbcbbdcac1c1a300c6d178ed3579017152
Eric Freeman <eric.freeman@extendhealth.com>
2014-10-20 22:53:12 -0600

    Toilet opens when level starts.

--- src/Assets/Scripts/Player.cs
+++ src/Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ namespace Assets.Scripts
     public class Player : MonoBehaviour,
         IListener<YouLoseMessage>
     {
+        public Sprite OpenMouth;
+        public Sprite Smiling;
+
         public float Speed = 5;
         private bool _isDead;
 
@@ -27,8 +30,11 @@ namespace Assets.Scripts
             transform.Translate(Input.GetAxisRaw("Horizontal") * Speed * Time.deltaTime, 0, 0);
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, -7.8f, 3.5f), -3.5f, 0);
 
-            if(Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.Space))
+            {
                 EventAggregator.SendMessage(new StartPoopingMessage());
+                GetComponent<SpriteRenderer>().sprite = OpenMouth;
+            }
         }
 
         public void Handle(YouLoseMessage message)
