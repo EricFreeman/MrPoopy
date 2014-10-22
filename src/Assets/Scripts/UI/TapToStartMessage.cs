842da70ed01b0f952ed66d001f84617869d39307
Eric Freeman <eric.freeman@extendhealth.com>
2014-10-22 16:42:33 -0600

    Added text that says tap to start that disappears when games starts.

---
+++ src/Assets/Scripts/UI/TapToStartMessage.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.EventHandler;
+using Assets.Scripts.EventHandler.Messages;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class TapToStartMessage : MonoBehaviour,
+        IListener<StartPoopingMessage>
+    {
+        void Start()
+        {
+            this.Register<StartPoopingMessage>();
+        }
+
+        void OnDestroy()
+        {
+            this.UnRegister<StartPoopingMessage>();
+        }
+
+        public void Handle(StartPoopingMessage message)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
